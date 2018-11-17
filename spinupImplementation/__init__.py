2720ad109f203268c32b4b70dd22a2fa0135f10b
Peter Chau <peter.chau.esq@gmail.com>
2018-11-17 15:19:08 -0700

    init commit. cleaned out spinup module. Going to comment the discrete action space VPG to better understand it

---
+++ spinupImplementation/__init__.py
@@ -0,0 +1,8 @@
+# Algorithms
+from spinupImplementation.algos.vpg.vpg import vpg
+
+# Loggers
+from spinupImplementation.utils.logx import Logger, EpochLogger
+
+# Version
+from spinupImplementation.version import __version__
